b0fef6eb10b4081c6da360c9ed69ebd9353c5da9
comoco <775324623@qq.com>
2024-11-19 15:27:47 +0800

    create project

---
+++ VideoPlayerDlg.h
@@ -0,0 +1,27 @@
+﻿
+// video_playerDlg.h
+//
+
+#pragma once
+
+class CVideoPlayerDlg : public CDialogEx
+{
+public:
+    CVideoPlayerDlg(CWnd *pParent = nullptr);
+
+#ifdef AFX_DESIGN_TIME
+    enum { IDD = IDD_VIDEO_PLAYER_DIALOG };
+#endif
+
+protected:
+    virtual void DoDataExchange(CDataExchange *pDX);
+
+protected:
+    HICON m_hIcon;
+
+    virtual BOOL OnInitDialog();
+    afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+    afx_msg void OnPaint();
+    afx_msg HCURSOR OnQueryDragIcon();
+    DECLARE_MESSAGE_MAP()
+};
